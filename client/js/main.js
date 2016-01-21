fbd3500211be4e24c993cd0a3314ec50238b1095
Ben Friedland <ben174@gmail.com>
2016-01-20 22:03:18 -0800

    Configurable board size, bomb count

--- client/js/main.js
+++ client/js/main.js
@@ -1,21 +1,33 @@
 var mc = {
     init: function() {
-        $.getJSON('/board', mc.drawGame);
+        var boardSize = 10;
+        var bombCount = 15;
+        $.getJSON('/board/' + boardSize + '/' + bombCount, mc.drawGame);
     },
     drawGame: function(data) {
         $(data).each(function(index, rowData) {
             var row = $("<tr>");
             $(rowData).each(function(i, value) {
                 var cell = $("<td>").attr('data-value', value).html('x');
-                $(cell).click(mc.cellClicked);
+                $(cell).mousedown(mc.cellClicked);
                 $(row).append(cell);
             });
             $("#board").append(row);
         });
     },
     cellClicked: function(e) {
-        $(e.target).addClass("clicked").text($(e.target).attr('data-value'));
-        console.log(e);
+        if(e.which==3) {
+            $(e.target).removeClass("clicked")
+                .addClass("flagged")
+                .html("&bull;");
+        } else {
+            $(e.target).removeClass("flagged")
+                .addClass("clicked")
+                .text($(e.target)
+                .attr('data-value'));
+            $(e.target).closest("td")
+                .addClass("clicked");
+        }
     }
 };
 $(mc.init);
--- server/server.py
+++ server/server.py
@@ -4,9 +4,9 @@ import pynsweeper
 
 app = Flask(__name__)
 
-@app.route("/board")
-def index():
-    return json.dumps(pynsweeper.create_board(10, 20))
+@app.route("/board/<int:board_size>/<int:bomb_count>")
+def index(board_size, bomb_count):
+    return json.dumps(pynsweeper.create_board(board_size, bomb_count))
 
 if __name__ == "__main__":
     app.run(debug=True)
