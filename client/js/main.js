4de332df032cb6bf535919581652b8867e211966
Ben Friedland <ben174@gmail.com>
2016-01-20 21:37:50 -0800

    Client started

--- client/js/main.js
+++ client/js/main.js
@@ -1,19 +1,21 @@
 var mc = {
-  init: function() {
-    $.getJSON('/board', mc.drawGame);
-  },
-  drawGame: function(data) {
-    $(data).each(function(index, rowData) {
-      console.log('fin to draw row');
-      var row = $("<tr>");
-      $(rowData).each(function(i, value) {
-        var cell = $("<td>").text(value);
-        $(row).append(cell);
-      });
-      console.log(row);
-      $("#board").append(row);
-      console.log($("#board"));
-    });
-  }
+    init: function() {
+        $.getJSON('/board', mc.drawGame);
+    },
+    drawGame: function(data) {
+        $(data).each(function(index, rowData) {
+            var row = $("<tr>");
+            $(rowData).each(function(i, value) {
+                var cell = $("<td>").attr('data-value', value).html('x');
+                $(cell).click(mc.cellClicked);
+                $(row).append(cell);
+            });
+            $("#board").append(row);
+        });
+    },
+    cellClicked: function(e) {
+        $(e.target).addClass("clicked").text($(e.target).attr('data-value'));
+        console.log(e);
+    }
 };
 $(mc.init);
