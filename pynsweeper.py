81e99e7460f0724fd0dacca338ce8eca815fcf9d
Ben Friedland <ben174@gmail.com>
2016-01-20 17:34:34 -0800

    Calculate heat

--- pynsweeper.py
+++ pynsweeper.py
@@ -4,11 +4,12 @@ import random
 
 def main():
     size = 10
-    bombs = 4
+    bombs = 20
     board = []
     for i in xrange(size):
-        board.append([0] * size)
+        board.append(['-'] * size)
     lay_bombs(board, bombs)
+    calc_heat(board)
     print_board(board)
 
 
@@ -16,15 +17,47 @@ def lay_bombs(board, bomb_count):
     bombs_laid = 0
     while bombs_laid < bomb_count:
         x, y = random.randrange(len(board)), random.randrange(len(board))
-        if not board[x][y]:
-            board[x][y] = 1
+        if board[x][y] != '*':
+            board[x][y] = '*'
             bombs_laid += 1
+
+def calc_heat(board):
+    size = len(board)
+    for y, row in enumerate(board):
+        for x, cell in enumerate(row):
+            if cell == '*':
+                continue
+            heat_count = 0
+            # left
+            if x > 0:
+                if board[y][x-1] == '*':
+                    heat_count += 1
+            # right
+            if x+1 < size:
+                if board[y][x+1] == '*':
+                    heat_count += 1
+            # up
+            if y > 0:
+                if board[y-1][x] == '*':
+                    heat_count += 1
+            # down
+            if y+1 < size:
+                if board[y+1][x] == '*':
+                    heat_count += 1
+            board[y][x] = heat_count
+
+
+
+
 
 
 
 def print_board(board):
     for row in board:
-        print row
+        row_str = ''
+        for cell in row:
+            row_str += str(cell) + ' '
+        print row_str
 
 # ------x------
 # -----x---x-x-
